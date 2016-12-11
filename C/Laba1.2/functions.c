f9ca90558fdf928bbc943c56d024ac997a4b04fe
Alexey Medvedev <medva1997@yandex.ru>
2016-12-11 17:57:44 +0300

    fixes

--- C/Laba1.2/functions.c
+++ C/Laba1.2/functions.c
@@ -4,183 +4,180 @@
 #include "functions.h"
 
 
-
 #define BAD_READ -10
 #define OK 0
 
 
 unsigned long long tick(void)
 {
-    unsigned long long d;
-
-    __asm__ __volatile__ ("rdtsc" : "=A" (d) );
-
-    return d;
+	unsigned long long d;
+	__asm__ __volatile__ ("rdtsc" : "=A" (d));
+	return d;
 }
 
 void count_file_length(int *array_length, FILE *file)
-{   
-    *array_length = 0;
-    int temp;
-    while ((fscanf(file, "%d", &temp) != EOF))
-        *array_length = *array_length + 1;
+{
+	*array_length = 0;
+	int temp;
+	while ((fscanf(file, "%d", &temp) != EOF))
+		*array_length = *array_length + 1;
 }
 
 int readfromfile(int *array_st, int *array_end, FILE *file)
 {
-    for (; array_st < array_end; array_st++)
-    {
-        if (fscanf(file, "%d", array_st) != 1)
-        {
-            return BAD_READ;
-        }
-    }
-    return OK;
+	for (; array_st < array_end; array_st++)
+	{
+		if (fscanf(file, "%d", array_st) != 1)
+		{
+			return BAD_READ;
+		}
+	}
+	return OK;
 }
 
 //Checked
-int filer_summ( const int *array, const int *array_end)
+int filer_summ(const int *array, const int *array_end)
 {
-    int full_summ=0;
+	int full_summ = 0;
 
-    //printf("filer_summ-->\n");
-    
-    if((array_end-array )<0)
-    {
-        //printf("BAD POINTERS in filer_summ exit, %p  %p\n",array,array_end);
-        return BAD_POINTERS; 
-    }
+	//printf("filer_summ-->\n");
 
-    while(array!=array_end)
-    {
-        full_summ+=*array;
-        array++;
-    }
+	if ((array_end - array) < 0)
+	{
+		//printf("BAD POINTERS in filer_summ exit, %p  %p\n",array,array_end);
+		return BAD_POINTERS;
+	}
 
-    //printf("filer_summ exit with: %d\n",full_summ);
-    return full_summ;
+	while (array != array_end)
+	{
+		full_summ += *array;
+		array++;
+	}
+
+	//printf("filer_summ exit with: %d\n",full_summ);
+	return full_summ;
 }
 
 //Checked
-int filter_counter( const int *array, const int *array_end, int full_summ)
+int filter_counter(const int *array, const int *array_end, int full_summ)
 {
-    //printf("filter_counter-->\n");
-    int k=0;
+	//printf("filter_counter-->\n");
+	int k = 0;
 
-    while(array!=array_end)
-    {
-        full_summ-=*array;
-        if(full_summ<*array)
-            k++;
-        array++;
-    }
-    
-    //printf("filter_counter exit with: %d\n",k);
-    return k;
+	while (array != array_end)
+	{
+		full_summ -= *array;
+		if (full_summ < *array)
+			k++;
+		array++;
+	}
+
+	//printf("filter_counter exit with: %d\n",k);
+	return k;
 }
 
 //Checked
-int filter_newarray(int *array, int *array_end,int *filtered,int *filtered_end,int full_summ)
+int filter_newarray(int *array, int *array_end, int *filtered, int *filtered_end, int full_summ)
 {
 
-    while((array!=array_end)&&(filtered_end != filtered))
-    {
-        full_summ-=*array;
-        if(full_summ<*array)
-        {
-            *filtered=*array;
-            //printf("COPIED %d , %d\n",*filtered, *array );
-            filtered++;    
-        }
-        array++;
-    }
-    //printf("ex\n");
-    if(filtered_end!=filtered)
-        return ERROR_LENGTH;
+	while ((array != array_end) && (filtered_end != filtered))
+	{
+		full_summ -= *array;
+		if (full_summ < *array)
+		{
+			*filtered = *array;
+			//printf("COPIED %d , %d\n",*filtered, *array );
+			filtered++;
+		}
+		array++;
+	}
+	//printf("ex\n");
+	if (filtered_end != filtered)
+		return ERROR_LENGTH;
+	else
+		return OK;
+}
+
+//Checked
+int filter(int *array, int *array_end, int **filtered, int **filtered_end)
+{
+	int status_work = OK;
+	int full_summ = filer_summ(array, array_end);
+	int k = filter_counter(array, array_end, full_summ);
+
+	*filtered = (int *) malloc(k * sizeof(int));
+
+
+	if (*filtered != NULL)
+	{
+		*filtered_end = *filtered + k;
+		filter_newarray(array, array_end, *filtered, *filtered_end, full_summ);
+	} 
     else
-        return OK;
-}
+	{
+		status_work = ERROR_MALLOC;
+	}
+	return status_work;
 
-//Checked
-int filter(int *array, int *array_end,int **filtered,int **filtered_end)
-{
-    int status_work=OK;
-    int full_summ=filer_summ(array, array_end);
-    int k=filter_counter(array, array_end,full_summ);
-    
-    *filtered = (int*) malloc(k * sizeof(int));
-
-
-    if (*filtered != NULL) 
-    {        
-        *filtered_end = *filtered + k;
-        filter_newarray(array, array_end,*filtered,*filtered_end,full_summ);
-    }
-    else 
-    {
-        status_work = ERROR_MALLOC;
-    }
-    return status_work;
-    
 }
 
 //Checked
 void printarray(int *array, int *array_end)
 {
-    //debug("9",6);
-    while(array!=array_end)
-    {
-        printf("%d ", *array);
-        array++;
-    }
-    printf("\n");
+	//debug("9",6);
+	while (array != array_end)
+	{
+		printf("%d ", *array);
+		array++;
+	}
+	printf("\n");
 }
 
 int compare_int_less(const void *val1, const void *val2)
 {
-    int *a = (int *) val1;
-    int *b = (int *) val2;
-    //printf("%d    %d\n ",*b,*a );
-    return *b - *a;
+	int *a = (int *) val1;
+	int *b = (int *) val2;
+	return *b - *a;
 }
 
-char* bin_search(char* array, char* array_end, char* val,int size,int (*comparator)(const void *, const void *))
+char *bin_search(char *array, char *array_end, char *val, int size, int (*comparator)(const void *, const void *))
 {
-     int n=(array_end-array)/size;
-    while(array<array_end)
-    {
-        //printf("SEARCH2\n");
-        //printf("                %d          %d\n",(array_end-array),0);
-       
-        char *med=array+n/2*size;
+	int n = 0;
+	char *med;
 
-        if(comparator(med,val)>0)
-            array=med+size;
-        else
-            array_end=med;
-        //printf("SEARCH3 %p  %p \n",array,array_end);
-    }
-    //printf("SEARCH4\n %p  %p %d %d\n",array,array_end,*array,val);
+	while (array < array_end)
+	{
+		n = (array_end - array) / size;
+		med = array + n / 2 * size;
 
-    return array;
+		if (comparator(med, val) > 0)
+			array = med + size;
+		else
+			array_end = med;
+	}
+
+	return array;
 }
 
 
-void mod_insertion_sort(void *adr,const size_t nmemb,const size_t size_of_el, int (*comparator)(const void*, const void*))
+void mod_insertion_sort(void *adr, const size_t nmemb, const size_t size_of_el,
+						int (*comparator)(const void *, const void *))
 {
-    char *array =(char *) adr;
-    char *array_tmp =(char *) adr;
-    char *tmp=malloc(size_of_el);
-    //char *array_end=array+size_of_el*nmemb;
-    char *pos;
-   
-    while(array!=array_tmp+size_of_el*nmemb)
-    {
-        memcpy(tmp,array,size_of_el);
-        pos=bin_search(array_tmp,array,tmp,size_of_el,comparator);
-        memmove(pos+size_of_el,pos,array-pos );
-        memcpy(pos,tmp,size_of_el);
-        array+=size_of_el;
-    }
-    free(tmp);
+	char *array = (char *) adr;
+	char *tmp = malloc(size_of_el);
+	char *pos;
+
+	while (array != adr + size_of_el * nmemb)
+	{
+		memcpy(tmp, array, size_of_el);
+
+		pos = bin_search(adr, array, tmp, size_of_el, comparator);
+		memmove(pos + size_of_el, pos, array - pos);
+		memcpy(pos, tmp, size_of_el);
+
+		array = array + size_of_el;
+		//printarray(adr,adr+size_of_el*nmemb);
+	}
+
+	free(tmp);
 }
--- C/Laba1.2/main.c
+++ C/Laba1.2/main.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "functions.h"
 
 #define ERROR_OPEN_INPUT_FILE -1
@@ -10,149 +11,191 @@
 #define OK 0
 
 #define DEBUG 10
+#define N 20
 
-void debug(char* str,int lev)
+void debug(char *str, int lev)
 {
-    if(lev>DEBUG)
-        printf("DEBUG MESS %s\n",str);
+	if (lev > DEBUG)
+		printf("DEBUG MESS %s\n", str);
 }
 
+
+int testing(int *a_st, int *a_end, int sort_t)
+{
+
+	unsigned long long t_st, t_end, full_time = 0;;
+	int size = a_end - a_st;
+
+	if (sort_t == 0)
+	{
+		printf("\nМодифицированная сортировка вставками с бин. поиском\n\n");
+	} else
+	{
+		printf("\nВстроенная быстрая сортировка сортировка\n\n");
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		int *filtered = malloc(sizeof(int) * size);
+		int *filtered_end = filtered + size;
+
+		memcpy(filtered, a_st, size * sizeof(int));
+
+		if (sort_t == 0)
+		{
+			t_st = tick();
+			mod_insertion_sort(filtered, size, 4, compare_int_less);
+			t_end = tick();
+		} 
+        else
+		{
+			t_st = tick();
+			qsort(filtered, size, 4, compare_int_less);
+			t_end = tick();
+		}
+
+
+		printf("test  %d 'time': \t%llu\n", i, (t_end - t_st));
+		full_time = full_time + (t_end - t_st);
+
+		free(filtered);
+
+	}
+
+	printf("Average  time : \t%llu\n", full_time / N);
+
+}
 
 
 int read_file(int **array, int *array_length, FILE *file)
-{	
+{
 
-    debug("1",6);
-    *array_length = 0;
-    int status_work = OK;
-    debug("2",6);
-    count_file_length(array_length, file);
-    debug("3",6);
+	debug("1", 6);
+	*array_length = 0;
+	int status_work = OK;
+	debug("2", 6);
+	count_file_length(array_length, file);
+	debug("3", 6);
 
-    rewind(file);
-    debug("4",6);
+	rewind(file);
+	debug("4", 6);
 
-    if (*array_length == 0) 
-    {
-        status_work = ERROR_LENGTH;
-    }
+	if (*array_length == 0)
+	{
+		status_work = ERROR_LENGTH;
+	} 
     else
-    {
-        //create array
-        *array = malloc(*array_length * sizeof(int));
-        if (*array != NULL) 
-        {            
-            int *p_end;
-            //count end poiner
-            p_end = *array + *array_length;
-            debug("5",6);
-            status_work = readfromfile(*array, p_end, file);
-        }
-        else 
-        {
-            status_work = ERROR_MALLOC;
-        }
-    }
-    return status_work;
-    debug("6",6);
+	{
+		//create array
+		*array = malloc(*array_length * sizeof(int));
+		if (*array != NULL)
+		{
+			int *p_end;
+			//count end poiner
+			p_end = *array + *array_length;
+			debug("5", 6);
+			status_work = readfromfile(*array, p_end, file);
+		} else
+		{
+			status_work = ERROR_MALLOC;
+		}
+	}
+	return status_work;
+	debug("6", 6);
 }
 
-void printarray_to_file(int *array, int *array_end,FILE *file)
+void printarray_to_file(int *array, int *array_end, FILE *file)
 {
-    debug("7",6);
-    while(array!=array_end)
-    {
-        fprintf(file,"%d ", *array);
-        array++;
-    }
+	debug("7", 6);
+	while (array != array_end)
+	{
+		fprintf(file, "%d ", *array);
+		array++;
+	}
 
 }
 
 int main(int argc, char **argv)
-{	
-	debug("10",6);
-    int array_length = 0;
-    int *array;
+{
+	debug("10", 6);
+	int array_length = 0;
+	int *array;
 
-    int status_work = OK;
-    
-    FILE * file;
-    
-    if (argc < 2) 
-    {
-    	
-        status_work = ERROR_ARG;
-    }
+	int status_work = OK;
+
+	FILE *file;
+
+	if (argc < 2)
+	{
+
+		status_work = ERROR_ARG;
+	} 
     else
-    {
-        file = fopen(*++argv, "r");
+	{
+		file = fopen(*++argv, "r");
 
-        if (file == 0)
-        {
-            status_work = ERROR_OPEN_INPUT_FILE;
-        }
-       
-    }
-    debug("11",6);
+		if (file == 0)
+		{
+			status_work = ERROR_OPEN_INPUT_FILE;
+		}
 
-    if (status_work == OK) 
-    {            
-    	debug("12",6);
-        status_work = read_file(&array, &array_length, file);     
-        debug("13",6);
+	}
+	debug("11", 6);
 
-        if (status_work == OK)
-        {
-            int *array_end;
-            //count end poiner
-            array_end = array + array_length;
-            debug("14",6);
-            printarray(array,array_end);
-            debug("15",6);
-            
-            int *filtered;
-            int *filtered_end;
-            
-            debug("16",6);
-            status_work=filter(array, array_end,&filtered,&filtered_end);;
-            debug("20",6);
-            printarray(filtered,filtered_end);
-            debug("21",6);
-            
-            //mod_insertion_sort(array, array_end);
-            unsigned long long tb, te;
-            //Среднеее 50 замеров
-            tb = tick();
-            mod_insertion_sort(filtered,filtered_end-filtered,4,compare_int_less);
-            te = tick();
-            printf("test 'time': %llu\n", (te - tb) );
-            debug("17",6);
-            //printarray(array, array_end);
-            printarray(filtered,filtered_end);
-            debug("18",6);
+	if (status_work == OK)
+	{
+		debug("12", 6);
+		status_work = read_file(&array, &array_length, file);
+		debug("13", 6);
+
+		if (status_work == OK)
+		{
+			int *array_end;
+			//count end poiner
+			array_end = array + array_length;
+			debug("14", 6);
+			printarray(array, array_end);
+			debug("15", 6);
+
+			int *filtered;
+			int *filtered_end;
+
+			debug("16", 6);
+			status_work = filter(array, array_end, &filtered, &filtered_end);;
+			debug("20", 6);
+			printarray(filtered, filtered_end);
+			debug("21", 6);
 
 
-        }    
+			testing(filtered, filtered_end, 0);
+			testing(filtered, filtered_end, 1);
 
-    }
+			debug("17", 6);
+			//printarray(array, array_end);
+			printarray(filtered, filtered_end);
+			debug("18", 6);
 
-    switch (status_work) 
-    {
-        case (ERROR_MALLOC):
-            printf("Can`t malloc memory.\n");
-            break;
-        case (ERROR_LENGTH):
-            printf("Empty file or zero length of the array.\n");
-            break;
-        case (ERROR_ARG):
-            printf("Not enough arguments.\n");
-            break;
-        case (ERROR_OPEN_INPUT_FILE):
-            printf("Unable to open input file.\n");
-            break;
-        
-    }
+		}
 
-    return status_work;
+	}
+
+	switch (status_work)
+	{
+		case (ERROR_MALLOC):
+			printf("Can`t malloc memory.\n");
+			break;
+		case (ERROR_LENGTH):
+			printf("Empty file or zero length of the array.\n");
+			break;
+		case (ERROR_ARG):
+			printf("Not enough arguments.\n");
+			break;
+		case (ERROR_OPEN_INPUT_FILE):
+			printf("Unable to open input file.\n");
+			break;
+
+	}
+
+	return status_work;
 
 }
